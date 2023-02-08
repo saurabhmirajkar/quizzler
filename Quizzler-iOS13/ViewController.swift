fac49683924efebea84c7513fec27ee4e0c65cd5
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-02-08 13:49:43 +0530

    added Question struct

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,18 +15,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let quiz = [
-        "Four + Two is equals to Six",
-        "Five - Thress is greater than One",
-        "Three + Eight is less than Ten"
+    let quiz : [Question] = [
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        
     ]
+    
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionText.text = quiz[0]
+        updateUI()
     }
-
+    
     @IBAction func answerButtonTapped(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let actualQuestion = quiz[questionNumber]
+        let actualAnswer = actualQuestion.answer
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong")
+        }
+        
+        questionNumber = (questionNumber + 1) % quiz.count
+        updateUI()
+        
+    }
+    
+    fileprivate func updateUI() {
+        questionText.text = quiz[questionNumber].text
     }
     
 }
